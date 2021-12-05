bbd9d34c26122898b3409e4a5c66dc461ac52c65
Slab-LED-Matrix-Pi <ruru.develop@gmail.com>
2021-12-05 16:11:47 +0900

    各表示行ごとに関数分離

--- bindings/python/Slab_ContentsPlaySystem/Slab_ContentsPlayer.py
+++ bindings/python/Slab_ContentsPlaySystem/Slab_ContentsPlayer.py
@@ -1,25 +1,27 @@
+#!/usr/bin/env python
 import time
 from samplebase import SampleBase
 from PIL import Image
 import threading
+import os
 
 #line_1　【表示1行目】16x32 RGBLED
 #line_2　【表示2行目】32x64 RGBLEDの「1行目 16x64」
 #line_3　【表示3行目】32x64 RGBLEDの「2行目 16x64」
 
 LINE_1_START_Y_POS:int = 0
-LINE_2_START_Y_POS:int = 16
+LINE_2_START_Y_POS:int = 8
+#16
 LINE_3_START_Y_POS:int = 32
 
 TEXT_SCROLL_SPEED = 0.02
 
-IMAGE_DIR_PASS = "/home/pi"
+IMAGE_DIR_PASS = "/home/pi/ElectricBoardContentsFolder"
 
 
 class ShowContents(SampleBase):
     def __init__(self, *args, **kwargs):
         super(ShowContents, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-i1", "--image1", help="The image to display", default="../../../examples-api-use/runtext.ppm")
 
         #表示するためのイメージデータが入る変数(RGBに変換後のもの)
         self.line_1_image = None
@@ -27,14 +29,17 @@ class ShowContents(SampleBase):
         self.line_3_image = None
 
     def load_image(self):
-        self.image1 = Image.open(self.args.image1).convert('RGB')
-        #self.image2 = Image.open(self.args.image2).convert('RGB')
+        number_of_files = sum(os.path.isfile(os.path.join(DIR, name)) for name in os.listdir(DIR))
+        self.line_1_image = Image.open(self.args.image1).convert('RGB')
+        #self.line_2_image = Image.open(self.args.image2).convert('RGB')
 
     def draw_image(self):
         image_buffer = self.matrix.CreateFrameCanvas()
         
         #描画部分
-        def line_1_draw_image(image_buffer, line_1_image):
+        def line_1_set_image(image_buffer, line_1_image):
+            #スクロール表示
+
             line1_x_pos = 0
             img_width, img_height = line_1_image.size
 
@@ -43,21 +48,58 @@ class ShowContents(SampleBase):
                 if (line1_x_pos > img_width):
                     line1_x_pos = 0
 
-                image_buffer.SetImage(self.image2, -line1_x_pos, LINE_1_START_Y_POS)
-                image_buffer.SetImage(self.image2, -line1_x_pos + img_width, LINE_1_START_Y_POS)
+                image_buffer.SetImage(line_1_image, -line1_x_pos, LINE_1_START_Y_POS)
+                image_buffer.SetImage(line_1_image, -line1_x_pos + img_width, LINE_1_START_Y_POS)
 
-                double_buffer = self.matrix.SwapOnVSync(double_buffer)
                 time.sleep(TEXT_SCROLL_SPEED)
 
-        def line_2_draw_image(self):
+        def line_2_set_image(image_buffer, line_2_image):
+            #切り替え表示
+
             line2_x_pos = 0
+            img_width, img_height = line_2_image.size
 
-        def line_3_draw_image(self):
+            while True:
+                line2_x_pos += 1
+                if (line2_x_pos > img_width):
+                    line2_x_pos = 0
+
+                image_buffer.SetImage(line_2_image, -line2_x_pos, LINE_2_START_Y_POS)
+                image_buffer.SetImage(line_2_image, -line2_x_pos + img_width, LINE_2_START_Y_POS)
+
+                time.sleep(TEXT_SCROLL_SPEED)
+
+        def line_3_set_image(image_buffer, line_3_image):
+            #スクロール表示
+
             line3_x_pos = 0
-        
-        thread = threading.Thread(target=line_1_draw_image, args=(image_buffer,self.line_1_image))
-        thread.start()        
+            img_width, img_height = line_3_image.size
 
+            while True:
+                line3_x_pos += 1
+                if (line3_x_pos > img_width):
+                    line3_x_pos = 0
+
+                image_buffer.SetImage(line_3_image, -line3_x_pos, LINE_3_START_Y_POS)
+                image_buffer.SetImage(line_3_image, -line3_x_pos + img_width, LINE_3_START_Y_POS)
+
+                time.sleep(TEXT_SCROLL_SPEED)
+
+        def all_line_draw_image(image_buffer):
+            while True:
+                image_buffer = self.matrix.SwapOnVSync(image_buffer)
+
+        thread_line_1 = threading.Thread(target=line_1_set_image, args=(image_buffer,self.line_1_image))
+        thread_line_1.start()
+
+        thread_line_2 = threading.Thread(target=line_2_set_image, args=(image_buffer,self.line_2_image))
+        thread_line_2.start()         
+
+        thread_line_3 = threading.Thread(target=line_3_set_image, args=(image_buffer,self.line_3_image))
+        thread_line_3.start()    
+
+        thread_line_draw = threading.Thread(target=all_line_draw_image, args=(image_buffer,))
+        thread_line_draw.start()        
 
     def run(self):
         self.load_image()
