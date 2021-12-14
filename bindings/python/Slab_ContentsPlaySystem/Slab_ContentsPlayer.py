d74593ab0100a96f1ff34304bb091be999f038bf
Slab-LED-Matrix-Pi <ruru.develop@gmail.com>
2021-12-14 12:21:56 +0900

    line3に関する処理を削除

--- bindings/python/Slab_ContentsPlaySystem/Slab_ContentsPlayer.py
+++ bindings/python/Slab_ContentsPlaySystem/Slab_ContentsPlayer.py
@@ -6,14 +6,12 @@ import threading
 import os
 from natsort import natsorted
 
-#line_1　【表示1行目】16x32 RGBLED
-#line_2　【表示2行目】32x64 RGBLEDの「1行目 16x64」
-#line_3　【表示3行目】32x64 RGBLEDの「2行目 16x64」
+
+#line_1　【表示1行目】32x64 RGBLEDの「1行目 16x64」
+#line_2　【表示2行目】32x64 RGBLEDの「2行目 16x64」
 
 LINE_1_START_Y_POS:int = 0
 LINE_2_START_Y_POS:int = 16
-#16
-LINE_3_START_Y_POS:int = 32
 
 TEXT_SCROLL_SPEED = 0.01
 
@@ -24,10 +22,9 @@ class ShowContents(SampleBase):
     def __init__(self, *args, **kwargs):
         super(ShowContents, self).__init__(*args, **kwargs)
 
-        #表示するためのイメージデータが入る変数(RGBに変換後のもの)
+        #表示するためのイメージデータ等のデータが入る変数(RGBに変換後のもの)
         self.line_1_image_etc = None
         self.line_2_image_etc = None
-        self.line_3_image_etc = None
 
     def load_image(self):
         """
@@ -38,7 +35,7 @@ class ShowContents(SampleBase):
         5.4までの処理を各Dir分行う
         """
         #ディレクトリのリスト
-        line_dir_list = ["Line1", "Line2", "Line3"]
+        line_dir_list = ["Line1", "Line2"]
         #各ディレクトリごとのイメージデータなどを格納したリスト
         display_information_data_dir = []
         #全ディレクトリのイメージデータなどを格納したリスト(多ネストリストになってる)
@@ -76,7 +73,6 @@ class ShowContents(SampleBase):
 
         self.line_1_image_etc = display_information_data[0]
         self.line_2_image_etc = display_information_data[1]
-        self.line_3_image_etc = display_information_data[2]       
 
         print(display_information_data[1])
 
@@ -136,28 +132,6 @@ class ShowContents(SampleBase):
 
                 time.sleep(TEXT_SCROLL_SPEED)
 
-        def line_3_set_image(image_buffer, line_3_image_etc):
-            #スクロール表示
-
-            selecting_drawing_image = 0
-            line3_x_pos = 0
-
-            while True:
-                line_3_image = line_3_image_etc[2][selecting_drawing_image]
-                img_width, img_height = line_3_image.size
-
-                line3_x_pos += 1
-                if (line3_x_pos > img_width):
-                    line3_x_pos = 0
-                    selecting_drawing_image += 1
-
-                image_buffer.SetImage(line_3_image, -line3_x_pos, LINE_3_START_Y_POS)
-                image_buffer.SetImage(line_3_image, -line3_x_pos + img_width, LINE_3_START_Y_POS)
-
-                image_buffer = self.matrix.SwapOnVSync(image_buffer)
-
-                time.sleep(TEXT_SCROLL_SPEED)
-
 
         def all_line_draw_image(image_buffer):
             while True:
@@ -168,10 +142,7 @@ class ShowContents(SampleBase):
         thread_line_1.start()
 
         thread_line_2 = threading.Thread(target=line_2_set_image, args=(image_buffer,self.line_2_image_etc))
-        thread_line_2.start()         
-
-        #thread_line_3 = threading.Thread(target=line_3_set_image, args=(image_buffer,self.line_3_image_etc))
-        #thread_line_3.start()    
+        thread_line_2.start()          
 
         thread_line_draw = threading.Thread(target=all_line_draw_image, args=(image_buffer,))
         thread_line_draw.start()        
